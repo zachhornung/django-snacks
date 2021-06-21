dca05b1fa8b093f62df91eff36acc5291199b3f0
Zach Hornung <Zachhornung@comcast.net>
2021-06-21 16:58:17 -0700

    everything works and tests pass

--- snacks_project/urls.py
+++ snacks_project/urls.py
@@ -18,5 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('snacks/', include('snacks.urls'))
+    path('snacks/', include('snacks.urls')),
+    path('', include('snacks.urls'))
 ]
